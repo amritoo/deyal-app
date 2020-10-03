8e77eb8c95deb19cf9a059dc1b7b528e3adf115f
amritoo <tipu.amrito@gmail.com>
2020-10-04 04:17:17 +0600

    Add Splash

--- src/main/java/app/deyal/deyal_app/DeyalApplication.java
+++ src/main/java/app/deyal/deyal_app/DeyalApplication.java
@@ -1,13 +1,23 @@
 package app.deyal.deyal_app;
 
+import app.deyal.deyal_app.data.Constants;
 import app.deyal.deyal_app.managers.DataManager;
 import app.deyal.deyal_app.managers.StageManager;
-import app.deyal.deyal_app.repository.Auth;
 import app.deyal.deyal_app.repository.PreferenceSave;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DeyalApplication extends Application {
+
+    public static boolean showLogin;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,12 +25,34 @@ public class DeyalApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        if (Auth.getUserData(PreferenceSave.getInstance().getToken())) {
-            DataManager.getInstance().token = PreferenceSave.getInstance().getToken();
-            StageManager.getInstance().createMainStage();
-            StageManager.getInstance().mainStage.show();
-        } else {
+        try {
+            showSplash();
+            if (showLogin) {
+                StageManager.getInstance().loginStage.show();
+            } else {
+                DataManager.getInstance().token = PreferenceSave.getInstance().getToken();
+                StageManager.getInstance().createMainStage();
+                StageManager.getInstance().mainStage.show();
+            }
+        } catch (Exception e) {
+            Logger.getLogger(Constants.LOG_NAME).log(Level.WARNING, "DeyalApplication:start", e);
             StageManager.getInstance().loginStage.show();
+        }
+    }
+
+    private void showSplash() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(("/app/deyal/deyal_app/views/splash.fxml")));
+            Parent root = loader.load();
+            Stage stage = new Stage(StageStyle.UNDECORATED);
+            Scene scene = new Scene(root);
+            StageManager.getInstance().setTheme(scene);
+            stage.setScene(scene);
+            StageManager.getInstance().setStageIcon(stage);
+            stage.setResizable(false);
+            stage.showAndWait();
+        } catch (IOException e) {
+            Logger.getLogger(Constants.LOG_NAME).log(Level.SEVERE, "DeyalApplication:showSplash", e);
         }
     }
 
---
+++ src/main/java/app/deyal/deyal_app/controllers/SplashController.java
@@ -0,0 +1,37 @@
+package app.deyal.deyal_app.controllers;
+
+import app.deyal.deyal_app.DeyalApplication;
+import app.deyal.deyal_app.repository.Auth;
+import app.deyal.deyal_app.repository.PreferenceSave;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class SplashController {
+
+    @FXML
+    public StackPane root;
+
+    @FXML
+    public void initialize() {
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(3), root);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(1);
+
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(3), root);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        fadeOut.setCycleCount(1);
+
+        fadeIn.play();
+        fadeIn.setOnFinished(event -> {
+            DeyalApplication.showLogin = !Auth.getUserData(PreferenceSave.getInstance().getToken());
+            fadeOut.play();
+        });
+
+        fadeOut.setOnFinished(event -> root.getScene().getWindow().hide());
+    }
+
+}
